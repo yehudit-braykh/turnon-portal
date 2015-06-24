0c162828fad6c433a6963a45c6d47711e058e5f3
Sebastian Acevedo <sebastian.acevedo@univtec.com>
2015-06-24 18:48:13 -0300

    env.php using heroku env variables

--- env.php
+++ env.php
@@ -1,18 +1,11 @@
 <?php
 
 // ENV FILE for UVOD-PORTAL
-define('ENVIRONMENT', 'local');
-
-define('UVOD_CONFIG', 'rjr');
-//define('UVOD_CONFIG', 'orbita');
-
-define('UVOD_API_USER', 'rjr_portal');
-//define('UVOD_API_USER', 'orbita_portal');
-
-define('UVOD_API_PASSWORD', 'Univtec1@');
-define('UVOD_API_ENDPOINT', 'http://uvod-api.univtec.com/index.php/');
-
-define('UVOD_THEME', 'rjr');
-//define('UVOD_THEME', 'orbita');
+define('ENVIRONMENT', ENV['UVOD_ENVIRONMENT']);
+define('UVOD_CONFIG', ENV['UVOD_CONFIG']);
+define('UVOD_API_USER', ENV['UVOD_API_USER']);
+define('UVOD_API_PASSWORD', ENV['UVOD_API_PASSWORD']);
+define('UVOD_API_ENDPOINT', ENV['UVOD_API_ENDPOINT']);
+define('UVOD_THEME', ENV['UVOD_THEME']);
 
 ?>
