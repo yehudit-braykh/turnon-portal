131145b650ce67bbf7522842662bd6f0c8050210
mariosabio <mario.sabio@univtec.com>
2015-10-08 16:49:18 -0300

    laseleccionmx theme was added

---
+++ application/views/themes/fbs-lsm/templates/footer.php
@@ -0,0 +1,65 @@
+
+        <!-- footer -->
+   <div class="footer_fs css_version_full">
+
+            <div class="foot_col1">
+
+                <a class="foot_logo" href="#">La Selección México</a>
+
+                <div>
+                    <a class="a1" href="http://laseleccion.com.mx/info/privacidad">Políticas de privacidad</a>
+                    <a class="a1" href="http://laseleccion.com.mx/info/terminosYCondiciones">Términos y Condiciones</a>
+                </div>
+
+            </div>
+
+
+            <div class="foot_col2">
+
+                <div class="foot_txt">
+                    <div class="div_p">La seleccion.com.mx es un sitio independiente., desarrollado por aficionados del Tri.</div>
+                    <div class="div_p peq">No guarda ninguna relación con la Federación Mexicana de Fútbol. Para ver el sitio oficial, ingresar <a href="http://miseleccion.mx/">aquí</a></div>
+                </div>
+                <div class="foot_links">
+                    <a href="http://brasil2014.bolavip.com/" target="_blank">Copa 2014</a>
+                    <a href="http://www.laseleccion.com.ar" target="_blank">La Selección Argentina</a>
+                    <a href="http://www.laseleccion.cl" target="_blank">La Selección Chile</a>
+                    <a href="http://www.laseleccion.com.ec" target="_blank">La Selección Ecuador</a>
+                    <a href="http://www.bolavip.com" target="_blank">BolaVip.com</a>
+                    <a href="http://bolavip.com/deportes/estadisticas/brasil2014/fixture" target="_blank">Fixture Mundial Brasil 2014</a>
+                </div>
+
+
+            </div>
+
+       <!--              <div class="foot_txt">
+                    <div class="div_p">La seleccion.com.mx es un sitio independiente., desarrollado por aficionados del Tri.</div>
+                    <div class="div_p peq">No guarda ninguna relación con la Federación Mexicana de Fútbol. Para ver el sitio oficial, ingresar <a href="http://miseleccion.mx/">aquí</a></div>
+
+
+                    <div class="foot_links">
+                    <a href="http://brasil2014.bolavip.com/" target="_blank">Copa 2014</a><a href="http://www.laseleccion.com.ar" target="_blank">La Selección Argentina</a><a href="http://www.laseleccion.cl" target="_blank">La Selección Chile</a><a href="http://www.laseleccion.com.ec" target="_blank">La Selección Ecuador</a><a href="http://www.bolavip.com" target="_blank">BolaVip.com</a><a href="http://bolavip.com/deportes/estadisticas/brasil2014/fixture" target="_blank">Fixture Mundial Brasil 2014</a>
+                </div>
+                </div>
+
+                
+
+            </div>
+
+    -->
+
+        </div>
+
+        <div class="foot_fs"></div>
+
+    </div>
+</div>
+
+        <!-- /footer -->
+
+
+<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
+<script type="text/javascript" src="<?php echo common_asset_url();?>js/jquery.bpopup.min.js"></script>
+</body>
+</html>
+
