675a3217f1e5bac5c8bdb6ee91e16cb3981491d7
Alejandro Miller <alejandro.miller@univtec.com>
2016-03-22 16:22:53 -0300

    epg timeline was updated

--- application/views/themes/rjr/templates/epg_timeline.php
+++ application/views/themes/rjr/templates/epg_timeline.php
@@ -13,98 +13,6 @@
     <tr>
 
         <?php
-//        if ($epg && isset($epg->content->entries)) {
-//            $h = 0;
-//
-//            foreach ($epg->content->entries as $value) {
-//
-//                if ($h == 0) {
-//                    $today = date('l M d, Y');
-//                    $day = date('l', ($value->{'startTime'} / 1000));
-//                    $date = date('M d, Y', ($value->{'startTime'} / 1000));
-//                    $currentDate = date('l M d, Y', ($value->{'startTime'} / 1000));
-//                    if ($today == $currentDate) {
-//                        echo '<td class="select_day daySelected" item-time="' . ($value->{'startTime'}) . '"><div id="on_today">ON TODAY</div><div class="today_content"><span class="epg_table_day">' . strtoupper($day) . '</span><br>' .
-//                        '<span class="epg_table_date">' . strtoupper($date) . '</span>' . '</div></td>';
-//                    }
-//                    $h++;
-//                } else {
-//                    $newDate = date('l M d, Y', ($value->{'startTime'} / 1000));
-//                    if ($currentDate != $newDate) {
-//                        $day = date('l', ($value->{'startTime'} / 1000));
-//                        $date = date('M d, Y', ($value->{'startTime'} / 1000));
-//                        echo '<td class="select_day" item-time="' . ($value->{'startTime'}) . '"><div class="date_content"><span class="epg_table_day">' . strtoupper($day) . '</span><br>' .
-//                        '<span class="epg_table_date">' . strtoupper($date) . '</span>' . '</div></td>';
-//                        $currentDate = $newDate;
-//                        $h++;
-//                    }
-//                }
-//            }
-//        }
-        ?>
-
-<!--    </tr>
-</table>
-<div id="epgData">
-    <table id="epgTable">-->
-        <?php
-//        $today = date('l M d, Y');
-//        $previous_title = '';
-//        if ($epg && isset($epg->content->entries) && sizeof($epg->content->entries) > 0) {
-//            for ($i = 0; $i < sizeof($epg->content->entries); $i++) {
-//
-//
-//                $currentDate = date('l M d, Y', ($epg->content->entries[$i]->{'startTime'} / 1000));
-//
-//                if ($today == $currentDate) {
-//                    $title_arr = explode('|', $epg->content->entries[$i]->{'program'}->{'title'});
-//
-//                    if ($country == 'Jamaica') {
-//                       
-//                        if ($title_arr[0] != '') {
-//                            $title = $title_arr[0];
-//                            if ($i == 0) {
-//                                echo '<tr class="current_row"><td class="epgTime current_epg_date">' . date('h:i A', ($epg->content->entries[$i]->{'startTime'} / 1000)) . '</td>'
-//                                . '<td class="epgData epgDataCurrent" style="border-right:8px solid #7f00bf;">' . '<span>' . $title . '</span></td></tr>';
-//                            } else {
-//                                echo '<tr><td class="epgTime">' . date('h:i A', ($epg->content->entries[$i]->{'startTime'} / 1000)) . '</td>'
-//                                . '<td class="epgData" id="' . $epg->content->entries[$i]->{'startTime'} . '">' . '<span>' . $title . '</span></td></tr>';
-//                            }
-//                 
-//                        }
-//                    } else {
-//                        if (isset($epg->content->entries[$i]->{'program'}->{'plprogram$runtime'}) &&
-//                                intval($epg->content->entries[$i]->{'program'}->{'plprogram$runtime'}) != 0 &&
-//                                sizeof($title_arr) > 1) {
-//
-//                            $title = $title_arr[1];
-//                        } else {
-//                            $title = $title_arr[0];
-//                        }
-//                        
-//                        if ($i == 0) {
-//                            echo '<tr class="current_row"><td class="epgTime current_epg_date">' . date('h:i A', ($epg->content->entries[$i]->{'startTime'} / 1000)) . '</td>'
-//                            . '<td class="epgData epgDataCurrent" style="border-right:8px solid #7f00bf;">' . '<span>' . $title . '</span></td></tr>';
-//                        } else {
-//                            echo '<tr><td class="epgTime">' . date('h:i A', ($epg->content->entries[$i]->{'startTime'} / 1000)) . '</td>'
-//                            . '<td class="epgData" id="' . $epg->content->entries[$i]->{'startTime'} . '">' . '<span>' . $title . '</span></td></tr>';
-//                        }
-//                    }
-//                }
-//            }
-//        }
-        ?>
-        <!--<script>-->
-<?php
-//if ($epg && $epg->content && $epg->content->entries)
-//    echo 'var contentEpg= ' . json_encode($epg->content->entries) . ';'
-    ?>
-<!--        </script>
-    </table>
-</div>-->
-
-
-<?php
         if ($epg && isset($epg->content->entries)) {
             $h = 0;
 
@@ -112,20 +20,20 @@
 
                 if ($h == 0) {
                     $today = date('l M d, Y');
-                    $day = date('l', ($value->{'pllisting$startTime'} / 1000));
-                    $date = date('M d, Y', ($value->{'pllisting$startTime'} / 1000));
-                    $currentDate = date('l M d, Y', ($value->{'pllisting$startTime'} / 1000));
+                    $day = date('l', ($value->startTime / 1000));
+                    $date = date('M d, Y', ($value->startTime / 1000));
+                    $currentDate = date('l M d, Y', ($value->startTime / 1000));
                     if ($today == $currentDate) {
-                        echo '<td class="select_day daySelected" item-time="' . ($value->{'pllisting$startTime'}) . '"><div id="on_today">ON TODAY</div><div class="today_content"><span class="epg_table_day">' . strtoupper($day) . '</span><br>' .
+                        echo '<td class="select_day daySelected" item-time="' . ($value->startTime) . '"><div id="on_today">ON TODAY</div><div class="today_content"><span class="epg_table_day">' . strtoupper($day) . '</span><br>' .
                         '<span class="epg_table_date">' . strtoupper($date) . '</span>' . '</div></td>';
                     }
                     $h++;
                 } else {
-                    $newDate = date('l M d, Y', ($value->{'pllisting$startTime'} / 1000));
+                    $newDate = date('l M d, Y', ($value->startTime / 1000));
                     if ($currentDate != $newDate) {
-                        $day = date('l', ($value->{'pllisting$startTime'} / 1000));
-                        $date = date('M d, Y', ($value->{'pllisting$startTime'} / 1000));
-                        echo '<td class="select_day" item-time="' . ($value->{'pllisting$startTime'}) . '"><div class="date_content"><span class="epg_table_day">' . strtoupper($day) . '</span><br>' .
+                        $day = date('l', ($value->startTime / 1000));
+                        $date = date('M d, Y', ($value->startTime / 1000));
+                        echo '<td class="select_day" item-time="' . ($value->startTime) . '"><div class="date_content"><span class="epg_table_day">' . strtoupper($day) . '</span><br>' .
                         '<span class="epg_table_date">' . strtoupper($date) . '</span>' . '</div></td>';
                         $currentDate = $newDate;
                         $h++;
@@ -146,27 +54,27 @@
             for ($i = 0; $i < sizeof($epg->content->entries); $i++) {
 
 
-                $currentDate = date('l M d, Y', ($epg->content->entries[$i]->{'pllisting$startTime'} / 1000));
+                $currentDate = date('l M d, Y', ($epg->content->entries[$i]->startTime / 1000));
 
                 if ($today == $currentDate) {
-                    $title_arr = explode('|', $epg->content->entries[$i]->{'pllisting$program'}->{'pl$title'});
+                    $title_arr = explode('|', $epg->content->entries[$i]->program->title);
 
                     if ($country == 'Jamaica') {
                        
                         if ($title_arr[0] != '') {
                             $title = $title_arr[0];
                             if ($i == 0) {
-                                echo '<tr class="current_row"><td class="epgTime current_epg_date">' . date('h:i A', ($epg->content->entries[$i]->{'pllisting$startTime'} / 1000)) . '</td>'
+                                echo '<tr class="current_row"><td class="epgTime current_epg_date">' . date('h:i A', ($epg->content->entries[$i]->startTime / 1000)) . '</td>'
                                 . '<td class="epgData epgDataCurrent" style="border-right:8px solid #7f00bf;">' . '<span>' . $title . '</span></td></tr>';
                             } else {
-                                echo '<tr><td class="epgTime">' . date('h:i A', ($epg->content->entries[$i]->{'pllisting$startTime'} / 1000)) . '</td>'
-                                . '<td class="epgData" id="' . $epg->content->entries[$i]->{'pllisting$startTime'} . '">' . '<span>' . $title . '</span></td></tr>';
+                                echo '<tr><td class="epgTime">' . date('h:i A', ($epg->content->entries[$i]->startTime / 1000)) . '</td>'
+                                . '<td class="epgData" id="' . $epg->content->entries[$i]->startTime . '">' . '<span>' . $title . '</span></td></tr>';
                             }
                  
                         }
                     } else {
-                        if (isset($epg->content->entries[$i]->{'pllisting$program'}->{'plprogram$runtime'}) &&
-                                intval($epg->content->entries[$i]->{'pllisting$program'}->{'plprogram$runtime'}) != 0 &&
+                        if (isset($epg->content->entries[$i]->program->runtime) &&
+                                intval($epg->content->entries[$i]->program->runtime) != 0 &&
                                 sizeof($title_arr) > 1) {
 
                             $title = $title_arr[1];
@@ -175,11 +83,11 @@
                         }
                         
                         if ($i == 0) {
-                            echo '<tr class="current_row"><td class="epgTime current_epg_date">' . date('h:i A', ($epg->content->entries[$i]->{'pllisting$startTime'} / 1000)) . '</td>'
+                            echo '<tr class="current_row"><td class="epgTime current_epg_date">' . date('h:i A', ($epg->content->entries[$i]->startTime / 1000)) . '</td>'
                             . '<td class="epgData epgDataCurrent" style="border-right:8px solid #7f00bf;">' . '<span>' . $title . '</span></td></tr>';
                         } else {
-                            echo '<tr><td class="epgTime">' . date('h:i A', ($epg->content->entries[$i]->{'pllisting$startTime'} / 1000)) . '</td>'
-                            . '<td class="epgData" id="' . $epg->content->entries[$i]->{'pllisting$startTime'} . '">' . '<span>' . $title . '</span></td></tr>';
+                            echo '<tr><td class="epgTime">' . date('h:i A', ($epg->content->entries[$i]->startTime / 1000)) . '</td>'
+                            . '<td class="epgData" id="' . $epg->content->entries[$i]->startTime . '">' . '<span>' . $title . '</span></td></tr>';
                         }
                     }
                 }
@@ -191,6 +99,6 @@
 if ($epg && $epg->content && $epg->content->entries)
     echo 'var contentEpg= ' . json_encode($epg->content->entries) . ';'
     ?>
-        </script>
+</script>
     </table>
 </div>
