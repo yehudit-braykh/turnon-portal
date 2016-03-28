1ff0d072883d079a7798d43c597f10cc58650287
luisriquelme <luis.riquelme@univtec.com>
2016-03-28 12:25:59 -0300

    live epg days breadcrums fix

--- assets/common/js/features/live_epg.js
+++ assets/common/js/features/live_epg.js
@@ -29,15 +29,14 @@ $(document).ready(function () {
         $(this).addClass('daySelected');
         selectDate = get_date($(this).attr('item-time'));
         $("#epgTable").html('');
-
         for (i = 0; i < contentEpg.length; i++) {
 
-            programDate = get_date(contentEpg[i].pllisting$startTime);
+            programDate = get_date(contentEpg[i].startTime);
 
             if (selectDate == programDate) {
 
-                hour = get_hour(contentEpg[i].pllisting$startTime);
-                title_arr = contentEpg[i].pllisting$program.pl$title.split('|');
+                hour = get_hour(contentEpg[i].startTime);
+                title_arr = contentEpg[i].program.title.split('|');
 
                 if (country_var == 'Jamaica') {
                     if (title_arr[0] != '') {
@@ -49,8 +48,8 @@ $(document).ready(function () {
                         }
                     }
                 } else {
-                    if (typeof contentEpg[i].pllisting$program.plprogram$runtime != 'undefined' &&
-                            contentEpg[i].pllisting$program.plprogram$runtime != 0 && title_arr.length > 1) {
+                    if (typeof contentEpg[i].program.runtime != 'undefined' &&
+                            contentEpg[i].program.runtime != 0 && title_arr.length > 1) {
                         title = title_arr[1];
                     } else {
                         title = title_arr[0];
