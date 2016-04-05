88cdb6f351fa3ef5404cd741afdfd826bada3043
Sebastian Acevedo <sebastian.acevedo@univtec.com>
2016-04-05 08:56:48 -0300

    key changed

--- application/views/themes/rjr/pages/vod_item.php
+++ application/views/themes/rjr/pages/vod_item.php
@@ -1,5 +1,7 @@
 <script type="text/javascript" src="https://content.jwplatform.com/libraries/07qZPa5L.js" ></script>
-<script>jwplayer.key = "BFr/jM6cxDTO5jdihqzp0fQ3Advd0Q8Fp6FUqw==";</script>
+<script>jwplayer.key = "wldzyhAXC/pV8hrmoKJJUJQUQU7UwoOXl6rN1w==";</script>
+
+
 
 <script type="text/javascript">
 
