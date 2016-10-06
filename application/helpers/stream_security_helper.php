72d2b8230363f60d2c9aa9c4fac241f761b1a4ec
Sebastian Acevedo <sebastian.acevedo@univtec.com>
2016-10-06 08:37:10 -0300

    Incrementing token duration to resolve network 400 issue

--- application/helpers/stream_security_helper.php
+++ application/helpers/stream_security_helper.php
@@ -6,10 +6,10 @@
 
         $base_url = substr($url, 0, strrpos($url, "/") + 1);
         $p = strlen($base_url);
-        $ci = 5;
-        $cd = 5;
+        $ci = 60;
+        $cd = 60;
         $e = time() + $expiration*60;
-        $cf = time() + 24*60*60;
+        $cf = time() + 240*60*60;
         $hash = md5($secret . $base_url . "?p=$p&ci=$ci&cd=$cd&e=$e&cf=$cf");
 
         return $url . "?p=$p&ci=$ci&cd=$cd&e=$e&cf=$cf&h=$hash";
