e0f2d642455f4cd56a96b137177904b04eaa26b4
ahmad327 <ahmad327@gmail.com>
2016-12-06 10:36:06 +0200

    ahmad fuckup 2.0

--- assets/theme/ped/js/controllers/pages/header.js
+++ assets/theme/ped/js/controllers/pages/header.js
@@ -1,5 +1,10 @@
 var h = null;
 peruDigitalApp.controller('headerController', function homeController ($scope) {
       h = $scope;
+      $scope.isLoggedIn= function(){
+          return false;
+      }
 
+      $scope.user={name:'Larry Vaughn',
+                   profile_pic:'/assets/theme/ped/images/static-images/profile-pic.png'};
   });
