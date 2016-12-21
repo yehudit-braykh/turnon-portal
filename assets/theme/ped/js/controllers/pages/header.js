4b3031b986a7972c2fd371c2c5651d6068ef8719
Mark Rozovsky <markroze@gmail.com>
2016-12-21 18:13:28 +0200

    code alignment

--- assets/theme/ped/js/controllers/pages/header.js
+++ assets/theme/ped/js/controllers/pages/header.js
@@ -30,24 +30,24 @@ peruDigitalApp.controller('headerController', function headerController ($scope,
           first_name:'',
           last_name:''
     };
-        $scope.logout = function (){
-            AuthService.logout();
-            $location.url('/');
-        }
-        $scope.loginUser = function () {
-            $scope.loginError=null;
-            AuthService.login($scope.userRegister.email, $scope.userRegister.password);
-        };
+    $scope.logout = function (){
+        AuthService.logout();
+        $location.url('/');
+    }
+    $scope.loginUser = function () {
+        $scope.loginError=null;
+        AuthService.login($scope.userRegister.email, $scope.userRegister.password);
+    };
 
-        $scope.registerUser = function () {
-            $scope.registerError=null;
-            //   $scope.userRegister.last_name = 'Rozo';
-            //   $scope.userRegister.first_name = 'Mark';
-            // console.log('here!!!', $scope.user );
-            if($scope.userRegister.password != $scope.userRegister.confirmPassword){
-                $scope.registerError="Password does not match confirm password!";
-            }else{
-                AuthService.register($scope.userRegister).then(function(data) {
+    $scope.registerUser = function () {
+        $scope.registerError=null;
+        //   $scope.userRegister.last_name = 'Rozo';
+        //   $scope.userRegister.first_name = 'Mark';
+        // console.log('here!!!', $scope.user );
+        if($scope.userRegister.password != $scope.userRegister.confirmPassword){
+            $scope.registerError="Password does not match confirm password!";
+        }else{
+            AuthService.register($scope.userRegister).then(function(data) {
             });
         }
     };
