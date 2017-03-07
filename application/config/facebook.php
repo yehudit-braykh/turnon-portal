3c0ad2281c3104421f7d163f53afcf24f96ebeff
System Administrator <root@Marks-MacBook-Air.local>
2017-03-07 20:49:58 +0200

    new

--- application/config/facebook.php
+++
@@ -1,5 +0,0 @@
-<?php
-if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-$config['appId'] = '1623813711226372';
-$config['secret'] = '86d226fc4bb64eef3ddab3a69bf523f8';
-?>
--- application/views/hauth/done.php
+++
@@ -1,8 +0,0 @@
-<script>
-// if (parent.location) parent.location.reload();
-// else if (window.opener) {
-	// window.opener.location.reload();
-	window.opener.finished_login();
-	window.close();
-// }
-</script>
