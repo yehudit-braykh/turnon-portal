fcca53ae43f060eae68df47f1d603f950149f3e9
Justin Podzimek <justin@macbook.local>
2017-03-22 11:03:46 -0600

    More style work on video permalink

--- assets/theme/clixtv/src/ui/video-permalink/controller.video-permalink.js
+++ assets/theme/clixtv/src/ui/video-permalink/controller.video-permalink.js
@@ -8,6 +8,8 @@
         'videosService',
         function($q, $scope, $timeout, $stateParams, videosService) {
 
+            $scope.expanded = false;
+
             $scope.seriesList = [
                 {
                     label: 'Series 1: Series Name Here...'
@@ -30,15 +32,30 @@
                 .then(
                     function onSuccess(data) {
                         $scope.video = data.data;
-                        console.log(data.data);
+                        $scope.nextVideos = [];
+                        $scope.relatedVideos = [];
+
+                        var i = 0, length = 12;
+                        for (i = 0; i < length; i++) {
+                            $scope.nextVideos.push(data.data);
+                        }
+                        for (i = 0; i < length; i++) {
+                            $scope.relatedVideos.push(data.data);
+                        }
                     }
                 );
 
             $scope.onPlayerReady = function(configs) {
-                $scope.playerHeight = configs.height - angular.element(document.getElementById('toggle-button-container')).outerHeight() - 20;
+                $scope.playerHeight = (configs.height - angular.element(document.getElementById('toggle-button-container')).outerHeight() - 20);
+                $scope.originalPlayerHeight = $scope.playerHeight;
                 $timeout(function() {
                     $scope.$apply();
                 });
+            };
+
+            $scope.onExpandToggle = function() {
+                $scope.expanded = !$scope.expanded;
+                $scope.playerHeight = ($scope.expanded) ? (angular.element(document.getElementById('about-video-container')).outerHeight()) : ($scope.originalPlayerHeight);
             };
         }
     ];
