9f6c93fa5c7a41cb9a11473f73f594144b2714e2
Justin Podzimek <justin@macbook.local>
2017-05-04 20:36:19 -0600

    Fixing model

--- assets/theme/clixtv/dist/main.js
+++ assets/theme/clixtv/dist/main.js
@@ -6782,12 +6782,14 @@ angular.module('clixtv').run(['$templateCache', function($templateCache) {
                     this.order = data.order;
                     this.videos = new VideoListModel(data.videos);
 
-                    if (data.content.ProfilePicture) {
-                        this.logo = data.content.ProfilePicture.downloadUrl;
-                    }
+                    if (data.content) {
+                        if (data.content.ProfilePicture) {
+                            this.logo = data.content.ProfilePicture.downloadUrl;
+                        }
 
-                    if (data.content.BackgroundImage) {
-                        this.headerImage = data.content.BackgroundImage.downloadUrl;
+                        if (data.content.BackgroundImage) {
+                            this.headerImage = data.content.BackgroundImage.downloadUrl;
+                        }
                     }
                 }
             }
--- assets/theme/clixtv/src/models/category-model.js
+++ assets/theme/clixtv/src/models/category-model.js
@@ -11,12 +11,14 @@
                     this.order = data.order;
                     this.videos = new VideoListModel(data.videos);
 
-                    if (data.content.ProfilePicture) {
-                        this.logo = data.content.ProfilePicture.downloadUrl;
-                    }
+                    if (data.content) {
+                        if (data.content.ProfilePicture) {
+                            this.logo = data.content.ProfilePicture.downloadUrl;
+                        }
 
-                    if (data.content.BackgroundImage) {
-                        this.headerImage = data.content.BackgroundImage.downloadUrl;
+                        if (data.content.BackgroundImage) {
+                            this.headerImage = data.content.BackgroundImage.downloadUrl;
+                        }
                     }
                 }
             }
