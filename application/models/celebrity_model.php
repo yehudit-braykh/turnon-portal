1d70b4e2389b7b00d7c164abe15be1cddb2e878b
Ahmad Abdalla <ahmad327@gmail.com>
2017-05-06 16:36:54 +0300

    celebrity content fix

--- application/models/celebrity_model.php
+++ application/models/celebrity_model.php
@@ -25,18 +25,33 @@ class Celebrity_model extends Uvod_Model {
 	public function get_celebrity($id){
 		if ($this->fastcache_model->get_cache("get_celebrity".$id))
 			return $this->fastcache_model->get_cache("get_celebrity".$id);
-		$data =  $this->celebrity_rows($this->apiCall('celebrity/'.$id.'/related')->entries)[0];
+		// $arr = array();
+		// array_push($arr, );
+		$data = $this->apiCall('celebrity/'.$id.'/related')->entries;
+		$data = $this->celebrity_rows($data)[0];
 		$this->fastcache_model->set_cache("get_celebrity".$id,$data);
 		return $data;
 	}
 
 	function celebrity_rows($items){
+		// debug($items);
 		foreach ($items as &$item) {
-			//debug($cat);
-
+			// debug($item);
 			if($item->series){
 				foreach ($item->series as &$serie) {
 					$serie->brands = $this->rows($serie->brands);
+
+					$serie->campaigns = $this->rows($serie->campaigns);
+					$serie->offers = $this->rows($serie->offers);
+
+					$arr = array();
+					array_push($arr, $serie->charity);
+					$serie->charity = $this->rows($arr)[0];
+
+					$arr = array();
+					array_push($arr, $serie->celebrity);
+					$serie->celebrity = $this->rows($arr)[0];
+
 					foreach ($serie->brands as &$brand) {
 						$brand->offers = $this->rows($brand->offers);
 					}
@@ -44,6 +59,11 @@ class Celebrity_model extends Uvod_Model {
 						foreach ($serie->seasons as &$season) {
 							foreach ($season->episodes as &$episode) {
 								$episode->brands = $this->rows($episode->brands);
+								$episode->campaigns = $this->rows($episode->campaigns);
+
+								$arr = array();
+								array_push($arr, $episode->charity);
+								$episode->charity = $this->rows($arr)[0];
 
 								$arr = array();
 								array_push($arr, $episode->celebrity);
@@ -56,12 +76,36 @@ class Celebrity_model extends Uvod_Model {
 			if($item->offers)
 				$item->offers = $this->rows($item->offers)[0];
 			if($item->charities)
-			if($item->brands)
+			if($item->brands){
+				foreach ($item->brands as &$brand) {
+					$brand->offers = $this->rows($brand->offers);
+				}
 				$item->brands = $this->rows($item->brands);
-			if($item->charities)
+			}
+
+			if($item->charities){
 				$item->charities = $this->rows($item->charities);
-			if($item->videos)
+			}
+
+			if($item->campaigns){
+				$item->campaigns = $this->rows($item->campaigns);
+			}
+
+			if($item->videos){
+				foreach ($item->videos as &$video) {
+					$video->brands = $this->rows($video->brands);
+					$video->campaigns = $this->rows($video->campaigns);
+
+					$arr = array();
+					array_push($arr, $video->charity);
+					$video->charity = $this->rows($arr)[0];
+
+					$arr = array();
+					array_push($arr, $video->celebrity);
+					$video->celebrity = $this->rows($arr)[0];
+				}
 				$item->videos = $this->rows($item->videos);
+			}
 
 		}
 
