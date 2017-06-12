dbe89313e0a92d640c77fb6f544c56b3e4362275
Ahmad Abdalla <ahmad327@gmail.com>
2017-06-12 12:58:34 +0300

    alexa script added

--- application/views/themes/clixtv/html.php
+++ application/views/themes/clixtv/html.php
@@ -60,6 +60,17 @@
         </script>
         <script src="/assets/theme/clixtv/dist/libs.min.js"></script>
         <script src="/assets/theme/clixtv/dist/main.min.js"></script>
+        <!-- Start Alexa Certify Javascript -->
+        <script type="text/javascript">
+        _atrk_opts =
+        { atrk_acct:"UA0fp1IWh91070", domain:"clixtv.com",dynamic: true}
+        ;
+        (function()
+        { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://d31qbv1cthcecs.cloudfront.net/atrk.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); }
+        )();
+        </script>
+        <noscript><img src="https://d5nxst8fruw4z.cloudfront.net/atrk.gif?account=UA0fp1IWh91070" style="display:none" height="1" width="1" alt="" /></noscript>
+        <!-- End Alexa Certify Javascript -->
 
     </body>
 </html>
