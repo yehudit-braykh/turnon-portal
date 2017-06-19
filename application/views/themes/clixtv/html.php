59530b381651d4ff40b4bcfc82f87f038982b53d
Ahmad Abdalla <ahmad327@gmail.com>
2017-06-19 12:06:57 +0300

    cache control headers and gzip compression added

--- application/views/themes/clixtv/html.php
+++ application/views/themes/clixtv/html.php
@@ -4,6 +4,9 @@
 
         <title ng-bind="pageTitle">ClixTV - Your Stars. Their Passions.</title>
 
+        <meta http-equiv="Expires" content="86400" />
+        <meta http-equiv="Cache-Control" content="max-age=86400" />
+
         <link rel="apple-touch-icon" sizes="180x180" href="assets/theme/clixtv/dist/images/apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="32x32" href="assets/theme/clixtv/dist/images/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="assets/theme/clixtv/dist/images/favicon-16x16.png">
