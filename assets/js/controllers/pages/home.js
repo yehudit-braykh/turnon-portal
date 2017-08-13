954c34105ba151f8b41f4a1bc9318a2ec8ebf6ad
yehudit <yehudit.golbraykh@univtec.com>
2017-08-13 23:51:40 +0300

    KINDS OF SPORTS start home page

--- assets/js/controllers/pages/home.js
+++ assets/js/controllers/pages/home.js
@@ -1,6 +1,39 @@
 var h = null;
 turnOnApp.controller('homeController', function homeController ($scope, $location, videoFactory, $http, $log,$interval) {
+  h = $scope;
 
+  $scope.sportLogos = [{title:'American football_1',
+                  url:'assets/images/categories/american_football_gray.png'},
+                  {title:'American football_2',
+                  url:'assets/images/categories/american_football_gray.png'},
+                  {title:'American football_3',
+                  url:'assets/images/categories/american_football_gray.png'},
+                  {title:'American football_4',
+                  url:'assets/images/categories/american_football_gray.png'},
+                  {title:'American football_5',
+                  url:'assets/images/categories/american_football_gray.png'},
+                  {title:'American football_6',
+                  url:'assets/images/categories/american_football_gray.png'},
+                  {title:'American football_7',
+                  url:'assets/images/categories/american_football_gray.png'},
+                  {title:'American football_8',
+                  url:'assets/images/categories/american_football_gray.png'},
+                  {title:'American footbal_9',
+                  url:'assets/images/categories/american_football_gray.png'},
+                  {title:'American football_10',
+                  url:'assets/images/categories/american_football_gray.png'},
+                  {title:'American football_11',
+                  url:'assets/images/categories/american_football_gray.png'},
+                  {title:'American footbal_12',
+                  url:'assets/images/categories/american_football_gray.png'},
+                  {title:'American football_13',
+                  url:'assets/images/categories/american_football_gray.png'},
+                  {title:'American football_14',
+                  url:'assets/images/categories/american_football_gray.png'},
+                  {title:'American football_15',
+                  url:'assets/images/categories/american_football_gray.png'},
+                  {title:'American football_16',
+                   url:'assets/images/categories/american_football_gray.png'}];
 
 
 });
