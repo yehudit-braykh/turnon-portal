100d10a7ca0a65854754c93fc3500a3a5d75c2c0
yehudit <yehudit.golbraykh@univtec.com>
2017-08-15 14:25:25 +0300

    fix

--- assets/theme/dist/main.js
+++ assets/theme/dist/main.js
@@ -3066,30 +3066,30 @@ angular.module('turnon').run(['$templateCache', function($templateCache) {
 
 (function() {
 
-    // var overview = function() {
-    //     return {
-    //         restrict: 'AE',
-    //         templateUrl: 'ui/account/overview/view.overview.html',
-    //         controller: 'AccountOverviewController'
-    //     }
-    // };
+    var overview = function() {
+        return {
+            restrict: 'AE',
+            templateUrl: 'ui/account/overview/view.overview.html',
+            controller: 'AccountOverviewController'
+        }
+    };
 
-    // var overviewInput = function() {
-    //     return {
-    //         restrict: 'AE',
-    //         replace: true,
-    //         templateUrl: 'ui/account/overview/view.overview-input.html',
-    //         controller: 'AccountOverviewInputController',
-    //         transclude: {
-    //             inputLabel: 'inputLabel'
-    //         },
-    //         scope: {
-    //             ngModel: '=',
-    //             onSave: '=',
-    //             type: '@'
-    //         }
-    //     }
-    // };
+    var overviewInput = function() {
+        return {
+            restrict: 'AE',
+            replace: true,
+            templateUrl: 'ui/account/overview/view.overview-input.html',
+            controller: 'AccountOverviewInputController',
+            transclude: {
+                inputLabel: 'inputLabel'
+            },
+            scope: {
+                ngModel: '=',
+                onSave: '=',
+                type: '@'
+            }
+        }
+    };
 
     angular.module('turnon')
         .directive('clixAccountOverview', overview)
--- assets/theme/src/ui/account/overview/directive.overview.js
+++ assets/theme/src/ui/account/overview/directive.overview.js
@@ -1,29 +1,29 @@
 (function() {
 
-    // var overview = function() {
-    //     return {
-    //         restrict: 'AE',
-    //         templateUrl: 'ui/account/overview/view.overview.html',
-    //         controller: 'AccountOverviewController'
-    //     }
-    // };
+    var overview = function() {
+        return {
+            restrict: 'AE',
+            templateUrl: 'ui/account/overview/view.overview.html',
+            controller: 'AccountOverviewController'
+        }
+    };
 
-    // var overviewInput = function() {
-    //     return {
-    //         restrict: 'AE',
-    //         replace: true,
-    //         templateUrl: 'ui/account/overview/view.overview-input.html',
-    //         controller: 'AccountOverviewInputController',
-    //         transclude: {
-    //             inputLabel: 'inputLabel'
-    //         },
-    //         scope: {
-    //             ngModel: '=',
-    //             onSave: '=',
-    //             type: '@'
-    //         }
-    //     }
-    // };
+    var overviewInput = function() {
+        return {
+            restrict: 'AE',
+            replace: true,
+            templateUrl: 'ui/account/overview/view.overview-input.html',
+            controller: 'AccountOverviewInputController',
+            transclude: {
+                inputLabel: 'inputLabel'
+            },
+            scope: {
+                ngModel: '=',
+                onSave: '=',
+                type: '@'
+            }
+        }
+    };
 
     angular.module('turnon')
         .directive('clixAccountOverview', overview)
