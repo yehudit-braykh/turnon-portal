1643cae126b61ab099cc2a9dae40891751661724
yehudit <yehudit.golbraykh@univtec.com>
2017-08-15 18:26:18 +0300

    home page

---
+++ assets/theme/src/models/home/home.js
@@ -0,0 +1,39 @@
+var h = null;
+turnOnApp.controller('homeController', function homeController ($scope, $location, videoFactory, $http, $log,$interval) {
+  h = $scope;
+
+  // $scope.sportLogos = [{title:'Soccer',
+  //                 url:'assets/images/categories/soccer_gray.png'},
+  //                 {title:'American football',
+  //                 url:'assets/images/categories/american_football_gray.png'},
+  //                 {title:'American football_3',
+  //                 url:'assets/images/categories/3_gray.png'},
+  //                 {title:'American football_4',
+  //                 url:'assets/images/categories/4_gray.png'},
+  //                 {title:'American football_5',
+  //                 url:'assets/images/categories/5_gray.png'},
+  //                 {title:'American football_6',
+  //                 url:'assets/images/categories/6_gray.png'},
+  //                 {title:'American football_7',
+  //                 url:'assets/images/categories/7_gray.png'},
+  //                 {title:'American football_8',
+  //                 url:'assets/images/categories/8_gray.png'},
+  //                 {title:'American footbal_9',
+  //                 url:'assets/images/categories/9_gray.png'},
+  //                 {title:'Golf',
+  //                 url:'assets/images/categories/golf_gray.png'},
+  //                 {title:'American football_11',
+  //                 url:'assets/images/categories/11_gray.png'},
+  //                 {title:'American footbal_12',
+  //                 url:'assets/images/categories/boxing_gray.png'},
+  //                 {title:'American football_13',
+  //                 url:'assets/images/categories/bike_gray.png'},
+  //                 {title:'American football_14',
+  //                 url:'assets/images/categories/horse_gray.png'},
+  //                 {title:'American football_15',
+  //                 url:'assets/images/categories/15_gray.png'},
+  //                 {title:'American football_16',
+  //                  url:'assets/images/categories/ski_gray.png'}];
+
+
+});
