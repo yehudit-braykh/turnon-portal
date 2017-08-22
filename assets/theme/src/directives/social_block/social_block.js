aee8a5a99babfae4e9e47c8cd99949a55f4b2268
yehudit <yehudit.golbraykh@univtec.com>
2017-08-22 12:04:42 +0300

    social block directive

---
+++ assets/theme/src/directives/social_block/social_block.js
@@ -0,0 +1,23 @@
+var so = null
+turnOnApp.directive('socialBlock', function() {
+    return {
+      restrict: 'E',
+      transclude: true,
+      scope: {
+          model: '=',
+          limit: "=",
+          order: "@",
+          reverse: "=",
+          link: "=",
+          search: '='
+      },
+      controller: ['$scope', '$location', function socialBlockController($scope, $location) {
+          so = $scope;
+          $scope.go = function (path) {
+              $location.path(path);
+          };
+
+      }],
+      templateUrl: '/assets/theme/src/directives/social_block/social_block.html',
+    };
+  })
